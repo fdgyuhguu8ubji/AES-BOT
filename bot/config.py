406eb78fea4b630d5181841fec3d449b214fa59f
fdgyuhguu8ubji <150494126+fdgyuhguu8ubji@users.noreply.github.com>
2025-07-28 18:54:37 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+API_ID = int(os.environ.get("API_ID", "26910777"))
+API_HASH = os.environ.get("API_HASH", "8601f2f24993f6fdbcbac3bb27ceec38")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-MONGO_URI = os.environ.get("MONGO_URI")
-FSUB_CHANNEL = int(os.environ.get("FSUB_CHANNEL"))  # Channel ID with -100
-BOT_USERNAME = os.environ.get("BOT_USERNAME")  # Without @
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://King:Cobra765592@cluster0.qy4m5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+FSUB_CHANNEL = int(os.environ.get("FSUB_CHANNEL", "-1002630250269"))  # Channel ID with -100
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "Demobaalveerrrebot")  # Without @
